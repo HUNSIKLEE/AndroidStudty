7f74e37945ff38608cdf8657acf7c5aae06b2763
{HUNSIKLEE} <hunsik0206@gmail.com>
2023-05-07 22:00:48 +0900

    feat:test

--- login/app/src/main/java/com/example/boardapp/LoginActivity.kt
+++ login/app/src/main/java/com/example/boardapp/LoginActivity.kt
@@ -16,15 +16,14 @@ class LoginActivity : AppCompatActivity() {
     lateinit var profileAdapter: ProfileAdapter
     val datas = mutableListOf<ProfileData>()
 
-    lateinit var editText: EditText
-    lateinit var btnadd: Button
-    lateinit var rvText: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        profileAdapter = ProfileAdapter(this)
+        binding.recyclerView.adapter = profileAdapter
 
 
         val shared = getSharedPreferences("login", MODE_PRIVATE)
@@ -46,65 +45,56 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initRecycler() {
-        profileAdapter = ProfileAdapter(this)
-        binding.recyclerView.adapter = profileAdapter
+
+        datas.apply {
+
+            add(
+                ProfileData(
+                    name = "charlse",
+                    email = "test@test.com",
+                    age = 4,
+                    img = R.drawable.charles
+                )
+            )
+            add(
+                ProfileData(
+                    name = "sik",
+                    email = "test@test.com",
+                    age = 34,
+                    img = R.drawable.ic_launcher_background
+                )
+            )
+            add(
+                ProfileData(
+                    name = "sung",
+                    email = "test@test.com",
+                    age = 35,
+                    img = R.drawable.ic_launcher_foreground
+                )
+            )
+            add(
+                ProfileData(
+                    name = "segi",
+                    email = "test@test.com",
+                    age = 34,
+                    img = R.drawable.charles
+                )
+            )
+            add(
+                ProfileData(
+                    name = "young",
+                    email = "test@test.com",
+                    age = 31,
+                    img = R.drawable.charles
+                )
+            )
 
 
-//
-//        datas.apply {
-//
-//            add(
-//                ProfileData(
-//                    name = "charlse",
-//                    email = "test@test.com",
-//                    age = 4,
-//                    img = R.drawable.charles
-//                )
-//            )
-//            add(
-//                ProfileData(
-//                    name = "sik",
-//                    email = "test@test.com",
-//                    age = 34,
-//                    img = R.drawable.ic_launcher_background
-//                )
-//            )
-//            add(
-//                ProfileData(
-//                    name = "sung",
-//                    email = "test@test.com",
-//                    age = 35,
-//                    img = R.drawable.ic_launcher_foreground
-//                )
-//            )
-//            add(
-//                ProfileData(
-//                    name = "segi",
-//                    email = "test@test.com",
-//                    age = 34,
-//                    img = R.drawable.charles
-//                )
-//            )
-//            add(
-//                ProfileData(
-//                    name = "young",
-//                    email = "test@test.com",
-//                    age = 31,
-//                    img = R.drawable.charles
-//                )
-//            )
-        val newData = ProfileData(
-            name = "New Name",
-            email = "new@test.com",
-            age = 25,
-            img = R.drawable.charles
-        )
-
-        datas.add(newData)
-        profileAdapter.datas = datas
-        profileAdapter.notifyItemInserted(datas.size - 1)
+            profileAdapter.datas = datas
+            profileAdapter.notifyItemInserted(datas.size - 1)
 //            profileAdapter.datas = datas
 //            profileAdapter.notifyItemRangeInserted(datas.size - 1)
         }
     }
+}
 
