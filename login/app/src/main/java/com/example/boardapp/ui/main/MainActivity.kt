3dd6dce1b9b0f29f7f1a9760ca51df8255aa067a
{HUNSIKLEE} <hunsik0206@gmail.com>
2023-06-24 14:53:11 +0900

    feat:   chronometer 사용 timer

--- login/app/src/main/java/com/example/boardapp/ui/main/MainActivity.kt
+++ login/app/src/main/java/com/example/boardapp/ui/main/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    
     private val profileAdapter = ProfileAdapter(
         { position, profileData ->
             showDetail(position,profileData)
